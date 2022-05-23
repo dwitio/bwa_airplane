363486e1585cea6b0c0fc52c8781ba04a04fbb2e
dwitio <tiothebook@gmail.com>
2022-05-23 19:59:05 +0700

    page cubit dan konfigurasi firebase

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:airplane/cubit/page_cubit.dart';
 import 'package:airplane/shared/theme.dart';
 import 'package:airplane/ui/pages/home_page.dart';
 import 'package:airplane/ui/pages/setting_page.dart';
@@ -5,14 +6,26 @@ import 'package:airplane/ui/pages/transaction_page.dart';
 import 'package:airplane/ui/pages/wallet_page.dart';
 import 'package:airplane/ui/widgets/custom_bottom_navigation_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Widget buildContent() {
-      return HomePage();
+    Widget buildContent(int currentIndex) {
+      switch (currentIndex) {
+        case 0:
+          return HomePage();
+        case 1:
+          return TransactionPage();
+        case 2:
+          return WalletPage();
+        case 3:
+          return SettingPage();
+        default:
+          return HomePage();
+      }
     }
 
     Widget customBottomNavigation() {
@@ -34,25 +47,35 @@ class MainPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               CustomButtomNavigationItem(
-                imageUrl: 'assets/icon_home.png',
-                isSelected: true,),
-              CustomButtomNavigationItem(imageUrl: 'assets/icon_booking.png',),
-              CustomButtomNavigationItem(imageUrl: 'assets/icon_card.png',),
-              CustomButtomNavigationItem(imageUrl: 'assets/icon_settings.png',),
+                index: 0,
+                imageUrl: 'assets/icon_home.png',),
+              CustomButtomNavigationItem(
+                index: 1,
+                imageUrl: 'assets/icon_booking.png',),
+              CustomButtomNavigationItem(
+                index: 2,
+                imageUrl: 'assets/icon_card.png',),
+              CustomButtomNavigationItem(
+                index: 3,
+                imageUrl: 'assets/icon_settings.png',),
             ],
           ),
         ),
       );
     }
 
-    return Scaffold(
-      backgroundColor: kBackgroundColor,
-      body: Stack(
-        children: [
-          buildContent(),
-          customBottomNavigation(),
-        ],
-      ),
+    return BlocBuilder<PageCubit, int>(
+      builder: (context, currentIndex) {
+        return Scaffold(
+          backgroundColor: kBackgroundColor,
+          body: Stack(
+            children: [
+              buildContent(currentIndex),
+              customBottomNavigation(),
+            ],
+          ),
+        );
+      },
     );
   }
 }
